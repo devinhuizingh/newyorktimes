23b3b5e58f1253f25b236290a787df4cd3692954
devinhuizingh <devinhuizingh@gmail.com>
2016-03-11 09:19:30 -0700

    initial commit

--- app/app.js
+++ app/app.js
@@ -1,3 +1,32 @@
-angular.module('calendarDemoApp', []);
-
-// your controller and directive code go here
+angular.module('calendarDemoApp', [])
+	.directive('calendar', function(){
+		return{
+			restrict: 'E',
+			templateUrl: 'template.html',
+			scope: '=',
+			//transclude: true,
+			link: function($scope, element, attrs) {
+			$scope.range = CalendarRange.getMonthlyRange(new Date());
+			$scope.days = $scope.range.days;
+			console.log($scope.range);
+			console.log($scope.range.days)
+				
+				
+			$scope.init = function () {
+				$scope.range = CalendarRange.getMonthlyRange(new Date('"'+$scope.yearModel+','+ $scope.monthModel+'"'));
+				$scope.days = $scope.range.days;
+				}
+			}
+			
+		}
+	})
+	
+	// .controller('cntrl',[
+	// 	'$scope',
+	// 	 function($scope) {
+			
+	// 		//console.log("controller works");
+	// 		//console.log(range);
+	// }])
+		
+	
